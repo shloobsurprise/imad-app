20fc193ec55b6d2941e027d0dbd1928fe6920acf
shloobsurprise <30623439+shloobsurprise@users.noreply.github.com>
2017-08-22 09:24:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,3 +22,20 @@ button.onclick = function(){
     request.open('GET', 'http://sooraj1990.imad.hasura-app.io/counter', true);
     request.send(null);
 };
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //Make request and send name
+    
+    //Capture and render
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
