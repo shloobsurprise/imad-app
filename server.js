42bb7ad3226dace44d92e573f64a465303e93509
shloobsurprise <30623439+shloobsurprise@users.noreply.github.com>
2017-08-22 08:32:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+  counter = counter +1;
+  res.send(counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  res.send(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/main.js', function (req, res) {
