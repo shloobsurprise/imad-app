d854f6bd875847c2e43936606a394ae4e348254e
shloobsurprise <30623439+shloobsurprise@users.noreply.github.com>
2017-08-22 09:39:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/ui/main.js', function (req, res) {
 var names = [];
 app.get('/submit-name/:name', function (req, res) {
   
-  var name = req.params.name;
+  var name = req.query.name;
   
   names.push(name);
   //JSON
