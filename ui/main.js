03a132e200a9beb259c6f7b314705bd16dcefa7d
shloobsurprise <30623439+shloobsurprise@users.noreply.github.com>
2017-08-22 08:02:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
-
+//changing main text
 var element = document.getElementById('main-text');
 element.innerHTML = "New value";
+
+//moving image
+var img = document.getElementById('img');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+}
